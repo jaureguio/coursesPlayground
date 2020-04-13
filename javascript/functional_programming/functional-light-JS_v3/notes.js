0a910c778412bca124ce5999c73f62ea38beae28
Oscar <oscarjaureguia@gmail.com>
2020-04-13 16:23:01 -0430

    include a functional_programming playground section

--- javascript/functional_programming/functional-light-JS_v3/notes.js
+++ javascript/functional_programming/functional-light-JS_v3/notes.js
@@ -1763,3 +1763,15 @@
 */
 
 
+/* Point-Free refactoring */
+// Given ajax(url, data, cb)
+
+var getPerson = partial(ajax, "http://some.api/person");
+var getLastOrder = partial(ajax, "http://some.api/order", {id: -1})
+
+//
+getLastOrder( function orderFound(order){
+  getPerson( { id: order.personId }, function personFound(person){
+    output( person.name );
+  } );
+} )
