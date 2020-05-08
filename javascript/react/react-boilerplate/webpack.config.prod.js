37306158656dda8ffbcf2862b4498c259991de72
Oscar <oscarjaureguia@gmail.com>
2020-05-08 02:04:05 -0400

    Allow loading of external dependencies when bundling for prod

--- javascript/react/react-boilerplate/webpack.config.prod.js
+++ javascript/react/react-boilerplate/webpack.config.prod.js
@@ -1,9 +1,16 @@
 const merge = require('webpack-merge')
 const baseConfig = require('./webpack.config.base')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 
 module.exports = merge(baseConfig, {
   mode: 'production',
-  devServer: {
-    port: 9000
+  plugins: [new BundleAnalyzerPlugin({
+    analyzerMode: 'static',
+    openAnalyzer: false,
+    reportFilename: 'bundle_sizes.html'
+  })],
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM'
   }
 })
