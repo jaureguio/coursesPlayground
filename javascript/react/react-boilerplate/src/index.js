f0ce7027121ab4dc202cbfb2c5158ab292db668f
Oscar <oscarjaureguia@gmail.com>
2020-05-15 03:52:36 -0400

    Include improved accessibility chekings with react-axe

--- javascript/react/react-boilerplate/src/index.js
+++ javascript/react/react-boilerplate/src/index.js
@@ -4,4 +4,13 @@ import '@babel/polyfill'
 import App from './App'
 import './styles.css'
 
-ReactDOM.render(<App />, document.querySelector('#root'))
+if (process.env.NODE_ENV === 'development') {
+  const axe = require('react-axe')
+  axe(React, ReactDOM, 1000)
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.querySelector('#root'))
