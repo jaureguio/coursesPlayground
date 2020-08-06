fa98047d571b5ea708cce2f4caa4232df3c2c801
Oscar <oscarjaureguia@gmail.com>
2020-08-06 02:32:40 -0400

    Configure webpack with css-loader and style-loader

--- javascript/react-typescript-starter/webpack.config.js
+++ javascript/react-typescript-starter/webpack.config.js
@@ -5,22 +5,27 @@ module.exports = {
   entry: './src/app/app.tsx',
   plugins: [
     new CleanWebpackPlugin({
-      cleanAfterEveryBuildPatterns: ['public/build']
+      cleanAfterEveryBuildPatterns: ['public/build'],
     }),
     new HtmlWebpackPlugin({
-      template: 'src/templates/index.html'
+      template: 'src/templates/index.html',
     }),
   ],
   output: {
     path: __dirname + '/public',
-    filename: 'build/[name].[contenthash].js'
+    filename: 'build/[name].[contenthash].js',
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js']
+    extensions: ['.ts', '.tsx', '.js'],
   },
   module: {
     rules: [
-      { test: /\.tsx?$/, loader: 'ts-loader' }
-    ]
-  }
+      { test: /\.tsx?$/, loader: 'ts-loader' },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+        exclude: /node_modules/,
+      },
+    ],
+  },
 }
