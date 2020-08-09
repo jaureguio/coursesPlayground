ca0c9afd67eadb6c2dc50302194d17447de195b3
Oscar <oscarjaureguia@gmail.com>
2020-08-09 01:42:04 -0400

    Include exercise implmentations file - advanced react hooks

---
+++ javascript/react/epic-react-kcd/03.advanced-react-hooks-master/src/exercise/my-implementations.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import '../styles.css'
+
+/**
+ *
+ *
+ * Exercise 01. useReducer: simple Counter
+ *
+ *
+ */
+
+ReactDOM.render(<App />, document.getElementById('root'))
