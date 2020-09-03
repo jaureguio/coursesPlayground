168f9d1bb2e1c9bb2c1fc7ab550fa9835a6f7813
Oscar <oscarjaureguia@gmail.com>
2020-09-03 02:28:16 -0400

    partial commit - epic react

--- javascript/react-typescript-starter/src/exercise/01.js
+++ javascript/react-typescript-starter/src/exercise/01.js
@@ -1,50 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import '../styles.css'
+// Simple Data-fetching
+// http://localhost:3000/isolated/exercise/01.js
 
-// const App: React.FC<{ compiler: string, framework: string }> = (props) => {
-  //   return (
-//     <div>
-//       <div>{props.compiler}</div>
-//       <div>{props.framework}</div>
-//     </div>
-//   );
-// }
-
-// ReactDOM.render(
-  //   <App compiler="TypeScript" framework="React" />,
-  //   document.getElementById("root")
-  // );
-
-/**
- *
- *
- * Exercise 01. Simple Data-fetching
- *
- *
- */
-
+import React from 'react'
 // 🐨 you'll also need to get the fetchPokemon function from ../pokemon:
-import { PokemonDataView, fetchPokemon } from '../pokemon'
+import {PokemonDataView} from '../pokemon'
 
 // 💰 use it like this: fetchPokemon(pokemonName).then(handleSuccess, handleFailure)
 
 // 🐨 create a variable called "pokemon" (using let)
-let pokemon
+
+// 💣 delete this now...
+const pokemon = {
+  name: 'TODO',
+  number: 'TODO',
+  attacks: {
+    special: [{name: 'TODO', type: 'TODO', damage: 'TODO'}],
+  },
+  fetchedAt: 'TODO',
+}
 
 // We don't need the app to be mounted to know that we want to fetch the pokemon
 // named "pikachu" so we can go ahead and do that right here.
 // 🐨 assign the pokemonPromise variable to a call to fetchPokemon('pikachu')
-const pokemonPromise = fetchPokemon('charizard')
 
 // 🐨 when the promise resolves, assign the "pokemon" variable to the resolved value
 // 💰 For example: somePromise.then(resolvedValue => (someValue = resolvedValue))
-pokemonPromise.then(pokemonData => pokemon = pokemonData) 
 
 function PokemonInfo() {
   // 🐨 if there's no pokemon yet, then throw the pokemonPromise
   // 💰 (no, for real. Like: `throw pokemonPromise`)
-  if(!pokemon) throw pokemonPromise
 
   // if the code gets it this far, then the pokemon variable is defined and
   // rendering can continue!
@@ -58,20 +42,15 @@ function PokemonInfo() {
   )
 }
 
-export default function App() {
+function App() {
   return (
     <div className="pokemon-info-app">
       <div className="pokemon-info">
         {/* 🐨 Wrap the PokemonInfo component with a React.Suspense component with a fallback */}
-        <React.Suspense fallback={<div>Loading...</div>}>
-          <PokemonInfo />
-        </React.Suspense>
+        <PokemonInfo />
       </div>
     </div>
   )
 }
 
-if (process.env.NODE_ENV !== 'test') {
-  // @ts-ignore
-  ReactDOM.unstable_createRoot(document.getElementById('root')).render(<App />)
-}
+export default App
